6fc5456073f0f2b146809360113c7986e91e9a6b
Aditya Sah <sah_aditya@yahoo.com>
2024-05-02 13:41:13 +0530

    Refactor code in gu-ito-1-6.py to calculate the minimum number of moves required to transform an array

--- gu-ito-1-6.py
+++ gu-ito-1-6.py
@@ -1,36 +1,92 @@
-'''Richard Hendricks, Gilfoyle, and Dinesh, the brilliant minds behind Pied Piper, are on a mission to optimize their messaging platform's data compression algorithm. They aim to represent "Yes" and "No" messages more efficiently by converting them into a different base that requires fewer characters, thus reducing the overall message size.
+'''You have been given an array ( a ) containing ( n ) positive integers.
 
-Richard explains that they currently represent "Yes" messages as 1 and "No" messages as 0 in their messaging platform. However, he believes they can further compress this representation by changing the base to a more efficient one, achieving a four-fold decrease in the number of characters needed to represent the messages.
+Your task is to find the minimum number of moves required to transform the array such that each element is divisible by ( k ) (given).
 
-Gilfoyle immediately jumps in, suggesting a strategy to change the base of the representation to achieve their goal. With his expertise in cryptography and data compression, Gilfoyle proposes a solution to map the binary representation onto a different base that requires only one-fourth of the characters needed in the existing base.
+During each move, you can either increase ( x ) by 1 or choose one element ( ai ) from ( a ) and increase it by ( x ) (then increase ( x ) by 1). However, you can only perform the first operation once for each ( i ) from 1 to ( n ).
 
-Help Gilfoyle defend his case and determine the optimal base for representing "Yes" and "No" messages to achieve a four-fold decrease in the number of characters required.
+Let's solve ( t ) independent test cases.
 
 Input:
 
-A single line containing a string made up of 0's and 1's representing "Yes" and "No" messages.
-Input Constraints: 1 ≤ N ≤ 57, where N is the number of characters (0's and 1's) in the string.
-
+The first line contains ( t ) (the number of test cases).
+For each test case: - The first line contains ( n ) and ( k ) (length of ( a ) and required divisor). - The second line contains ( n ) integers ( a1, a2, ..., an ).
 Output:
 
-Single line denoting the value of the binary string in a base that uses four times fewer characters than the existing base.
+For each test case, print the minimum number of moves required.
+Constraints:
 
+( 1 <= t <= 2 times 10^4 )
+( 1 <= n <= 2 times 10^5 )
+( 1 <= k <= 10^9 )
+( 1 <= ai <= 10^9 )
 Sample input:
 
-10110111000010010011011001010111111000111010010010000
+5
+4 3
+1 2 1 3
+10 6
+8 7 1 8 3 7 5 10 8 9
+5 10
+20 100 50 20 100500
+10 25
+24 24 24 24 24 24 24 24 24 24
+8 8
+1 2 3 4 5 6 7 8
 
 Sample output:
 
-16e126cafc7490'''
+6
+18
+0
+227
+8'''
 
 
-a = input()
+class RemainderCalculator:
+    def __init__(self, n, k, a):
+        self.n = n
+        self.k = k
+        self.a = a
 
-def base(b):
-    basesn=hex(int(b,2))[2:]
-    return basesn
-
-print(base(a))
+    def calculate_remainders(self):
+        rem = {}
+        #..... YOUR CODE STARTS HERE .....
     
+        for num in self.a:
+           remainder=num%self.k
+           if remainder!=0:
+               rem[remainder]=rem.get(remainder,0)+1
+    
+        #..... YOUR CODE ENDS HERE .....
+
+        return rem
+
+    def find_maximum_result(self, rem):
+         #..... YOUR CODE STARTS HERE .....
+
+        result=0
+        for r, count in rem.items():
+            moves=(self.k*(count-1))+(self.k-r)+1
+            result=max(result,moves)
+        return result
+        #..... YOUR CODE ENDS HERE .....
 
 
+if __name__ == "__main__":
+    from sys import stdin, stdout, setrecursionlimit
+
+    setrecursionlimit(10**6)
+
+    t = int(stdin.readline())
+    for _ in range(t):
+        n, k = map(int, stdin.readline().split())
+        a = list(map(int, stdin.readline().split()))
+
+        #..... YOUR CODE STARTS HERE .....
+
+        calc=RemainderCalculator(n,k,a)
+        remainders=calc.calculate_remainders()
+        result=calc.find_maximum_result(remainders)
+        stdout.write(str(result)+"\n")
+
+        #..... YOUR CODE ENDS HERE .....
